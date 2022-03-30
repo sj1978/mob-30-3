bdc2a7274f2ba456ab812b7b45e014a824a2cc19
Anibal Amoroso <daniel_amoroso31@hotmail.com>
2022-03-30 11:00:33 -0300

    Create index.rb

---
+++ index.rb
@@ -0,0 +1,17 @@
+def viralAdvertising(n)
+    
+some = 2 
+i = 2
+result = 2
+while i <= n 
+     some = (some * 3 / 2).floor
+    result  = result + some
+    i += 1
+  
+    
+end
+result
+end
+
+
+viralAdvertising(3)
